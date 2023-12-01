c52ef1217c936b0f231ad221544cab8c606305c1
agrisss <glffggga@gmail.com>
2023-12-01 21:57:13 +0200

    first commit

---
+++ react/src/App.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+
+
+  const addTask = () => {
+    if (taskInput.trim() !== '') {
+      setTasks([...tasks, { text: taskInput, completed: false }]);
+      setTaskInput('');
+    }
+  };
+
+  const checkTask = (index: number) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index: number) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="App">
+      <h2>To Do List</h2>
+      <div>
+        <input
+          type="text"
+          value={taskInput}
+          onChange={(e) => setTaskInput(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index} className={task.completed ? 'done' : ''}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => checkTask(index)}
+            />
+            <span>{task.text}</span>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
